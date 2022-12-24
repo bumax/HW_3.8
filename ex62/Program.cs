e369b909d9dfc5dc41a08e14714bc70cf368aca0
BuMax <max@butenko.in>
2022-12-25 02:58:20 +1000

    Exercise #62 completed

---
+++ ex62/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+Console.Write("Введите количество строк: ");
+var rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+var cols = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+
+void FillSpiralArray(int[,] array, int m, int n, int dir)
+{
+    //dir - направление движения (0 - вправо, 1 - вниз, 2 - влево, 3 - вверх)
+
+
+    while (IsValidPoint(array, m, n))
+    {
+        array[m, n] = i;
+        i++;
+        switch (dir)
+        {
+            case 0:
+                n++;
+                break;
+            case 1:
+                m++;
+                break;
+            case 2:
+                n--;
+                break;
+            case 3:
+                m--;
+                break;
+        }
+    }
+
+    if (array.Length == i - 1)
+        return;
+
+    switch (dir)
+    {
+        case 0:
+            m++;
+            n--;
+            FillSpiralArray(array, m, n, 1);
+            break;
+        case 1:
+            m--;
+            n--;
+            FillSpiralArray(array, m, n, 2);
+            break;
+        case 2:
+            m--;
+            n++;
+            FillSpiralArray(array, m--, n++, 3);
+            break;
+        case 3:
+            m++;
+            n++;
+            FillSpiralArray(array, m++, n--, 0);
+            break;
+    }
+
+}
+
+// проверям точку
+bool IsValidPoint(int[,] array, int m, int n)
+{
+    if (array.GetLength(0) > m && array.GetLength(1) > n && m >= 0 && n >= 0)
+        return array[m, n] == 0;
+    else
+        return false;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] testArray = new int[rows, cols];
+FillSpiralArray(testArray, 0, 0, 0);
+Print2DArray(testArray);
